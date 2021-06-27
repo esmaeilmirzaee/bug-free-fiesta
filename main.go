a58f325a8a2aca28c2328d921b2dfe912cd3afd9
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-06-27 21:20:56 +0430

    Play rps--log result

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-
 	"log"
+	"myapp/rps"
 	"net/http"
 	"text/template"
 )
@@ -12,22 +12,33 @@ const (
 )
 
 func main() {
+	http.HandleFunc("/play", playRound)
 	http.HandleFunc("/", homePage)
 	log.Println("Server is listening on", PORT)
 	http.ListenAndServe(":8000", nil)
 }
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	t, err:=template.ParseFiles("index.html")
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
+func playRound(w http.ResponseWriter, r *http.Request) {
+	winner, computerChoice, roundResult := rps.PlayRound(1)
+	log.Println(winner, computerChoice, roundResult)
+}
 
-	err=t.Execute(w,nil)
-	
-	if err!=nil {
+func homePage(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "index.html")
+}
+
+func renderTemplate(w http.ResponseWriter, page string) {
+	w.Header().Set("Content-Type", "text/html")
+	t, err := template.ParseFiles(page)
+	logErrors(err)
+
+	err = t.Execute(w, nil)
+	logErrors(err)
+
+}
+
+func logErrors(err error) {
+	if err != nil {
 		log.Fatal(err)
 		return
 	}
