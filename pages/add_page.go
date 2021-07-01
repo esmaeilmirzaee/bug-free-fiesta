c1210a7da222209e2b8f73f5c07db299e80c1df0
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-07-01 20:21:18 +0430

    Add new task redirect to home page

--- pages/add_page.go
+++ pages/add_page.go
@@ -1,12 +1,27 @@
 package pages
 
 import (
-	"fmt"
-	"log"
+	"myapp/task"
 	"net/http"
 )
 
 func AddPage(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.)
-	fmt.Fprint(w, "Add a task")
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	t := task.Task{
+		Id:      task.GenerateId(),
+		Title:   r.FormValue("task_title"),
+		Content: r.FormValue("task_content"),
+	}
+
+	if ok := task.AddTask(t); ok {
+		http.Redirect(w, r, "/", http.StatusCreated)
+		// fmt.Fprint(w, "Add a task", t)
+	} else {
+		http.Error(w, err.Error(), http.StatusNotAcceptable)
+	}
+
 }
