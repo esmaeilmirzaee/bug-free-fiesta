e604947c1c083d7f824570bcf43ba1478da1938e
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-07-01 16:37:52 +0430

    Methods

--- auth/user.go
+++ auth/user.go
@@ -1,5 +1,7 @@
 package auth
 
+import "fmt"
+
 type User struct {
 	Id                    int
 	Name, Email, Password string
@@ -14,4 +16,16 @@ type Group struct {
 func PromoteUser(u User) bool {
 	u.Admin = true
 	return true
+}
+
+func UpdateEmail(u *User, e string) {
+	u.Email = e
+}
+
+func (u User) Describe() string {
+	return fmt.Sprintf("User %s owns %s address. Is s/he admin? %t", u.Name, u.Email, u.Admin)
+}
+
+func (g Group) Describe() string {
+	return fmt.Sprintf("The current group has about %d users and it is enabled: %t", len(g.Users), g.Enabled)
 }
--- main.go
+++ main.go
@@ -9,6 +9,16 @@ import (
 func main() {
 	u1 := auth.User{Id: rand.Intn(1000), Name: "Esmaeil MIRZAEE", Email: "em@gmail.com", Password: "123", Admin: false}
 
+	u2 := auth.User{Id: rand.Intn(1000), Name: "Samuel Russell", Email: "samrus@g.com", Password: "123"}
+
+	var users []auth.User
+	users = append(users, u1, u2)
+	groupOne := auth.Group{Users: users, Enabled: true}
+
+	log.Println(groupOne.Describe())
+	log.Println(u1.Describe())
 	auth.PromoteUser(u1)
+	log.Println(u1)
+	auth.UpdateEmail(&u1, "me@gmail.com")
 	log.Println(u1)
 }
