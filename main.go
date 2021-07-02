cec3a59b1dc7e896de19de631557c81e78fc2b04
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-07-02 21:58:51 +0430

    challange-map; truncate float number

--- main.go
+++ main.go
@@ -1,32 +1,39 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"math"
 	"math/rand"
-	"strconv"
 	"time"
 )
 
 func main() {
-	var scores [5]float32
+	var scores [5]float64
 	rand.Seed(time.Now().UnixNano())
-	scores[0] = rand.Float32() * 100
-	scores[1] = rand.Float32() * 100
-	scores[2] = rand.Float32() * 100
-	scores[3] = rand.Float32() * 100
-	scores[4] = rand.Float32() * 100
-
-	// log.Printf("%.2f", scores)
-	for _, value := range scores {
-		str := fmt.Sprintf("%.2f", value)
-		log.Println("str->", str)
-		if temp, err := strconv.ParseFloat(str, 32); err != nil {
-			log.Println(err.Error())
-		} else {
-			log.Println(temp)
-		}
-		// tmp := strconv.FormatFloat(temp, 64, 2, 64)
-		// log.Println(tmp)
+	for i := 0; i < 5; i++ {
+		scores[i] = rand.Float64() * 100
 	}
+
+	for idx, value := range scores {
+		scores[idx] = toFixed(value, 2)
+	}
+
+	log.Println(average(scores))
+}
+
+func round(num float64) int {
+	return int(num + math.Copysign(0.5, num))
+}
+
+func toFixed(num float64, precision int) float64 {
+	output := math.Pow(10, float64(precision))
+	return float64(round(num*output)) / output
+}
+
+func average(scores [5]float64) float64 {
+	var total float64
+	for _, score := range scores {
+		total += score
+	}
+	return total / float64(len(scores))
 }
